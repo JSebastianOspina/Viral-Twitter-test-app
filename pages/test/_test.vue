5953dd8a9305864dfe7425175c8dbfa210ab1cfd
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2021-01-14 21:50:45 -0600

    without waves

---
+++ pages/test/_test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col h-screen w-full">
+      <div class="flex-auto">
+          <div class="bg-cover w-full h-full flex flex-col justify-center" style="background-image:url('/images/pattern.png')">
+            <div class="flex justify-center">
+                <img src="/images/logo.png" class="w-40 h-40" alt="logo">
+            </div>
+            <div>
+                <h1 class="text-5xl text-teal-400 text-center font-semibold">
+                    El Bunny
+                </h1>
+            </div>
+          </div>
+      </div>
+      <div class="flex-auto" style="background-color:#1F1147">
+          <p class="text-center font-semibold text-white text-3xl"> 
+              Let's Play!
+          </p>
+          <p class="text-center font-semibold text-white text-xl"> 
+              Play now and Level up
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
